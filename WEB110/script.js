dcc3d04be52c633e2c8fe7fccc6b5d649c885625
Ahmedullah Syed <123043793+asyed22@users.noreply.github.com>
2023-12-04 07:03:13 -0600

    wefwef2f

---
+++ WEB110/script.js
@@ -0,0 +1,19 @@
+function setTime() {
+    var time = new date();                
+    var HRS = time.getHours();
+    var MIN = time.getMinutes();
+    var SEC = time.getSeconds();
+    var session  = document.getElementById('session');
+    HRS = document.getElementById('HRS').innerHTML;
+    MIN = document.getElementById('MIN').innerHTML;
+    SEC = document.getElementById('SEC').innerHTML;
+            
+    if (hours > 12){
+        session.innerHTML = 'PM';
+        HRS = HRS - 12;
+    }
+    else{
+        session.innerHTML = 'AM';
+    }                    
+}
+setInterval(setTime,10);
