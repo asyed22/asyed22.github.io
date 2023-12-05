6f08903025aada7ef6a12b05412e87a58af6f8e1
Ahmedullah Syed <123043793+asyed22@users.noreply.github.com>
2023-12-05 02:38:02 -0600

    qewefwf

--- WEB110/script.js
+++ WEB110/script.js
@@ -1,18 +1,23 @@
-function setTime () {
-    var time = new date(ge);                
-    var HRS = document.getElementById('hours');
-    var MIN = document.getElementById('minutes');
-    var SEC = document.getElementById('seconds');
-    var session  = document.getElementById('session');
-            
-    if (HRS >= 12){
-        session.innerHTML = 'PM';
-        HRS = HRS - 12;
+function tick(){
+    var monthNames = ["January","Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const d = new Date();
+    document.getElementById('date').innerHTML = (dayNames[d.getDay()] + ' ' + d.getDate() + ' ' + monthNames[d.getMonth()] + ' ' +d.getFullYear());
+
+    var h = d.getHours();
+    var m = d.getMinutes();
+    var s = d.getSeconds();
+    var day = h<11 ? 'AM' : 'PM';
+    h = h<10? '0' +h: h;
+    m = m<10? '0' +m: m;
+    s = s<10? '0' +s: s;
+    if (h >= 12){
+        document.getElementById('session').innerHTML = 'PM';
+        h = h - 12;
     }else{
-        session.innerHTML = 'AM';
+        document.getElementById('session').innerHTML = 'AM';
     }
-    HRS.innerHTML = time.getHours();
-    MIN.innerHTML = time.getMinutes();
-    SEC.innerHTML = time.getSeconds();                    
-}
-setInterval(setTime , 1000);
+    document.getElementById('hours').innerHTML = h;
+    document.getElementById('minutes').innerHTML = m;
+    document.getElementById('seconds').innerHTML = s;
+} var inter = setInterval (tick , 400);
